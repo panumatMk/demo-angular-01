152f508c4d2fb95554588a915ee4eeacf27d63ad
panumatMk <panumat.mk@gmail.com>
2022-04-16 18:00:59 +0700

    [update] install redux dev tool

--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -10,6 +10,8 @@ import {MenuComponent} from './menu/menu/menu.component';
 import {StoreModule} from '@ngrx/store';
 import {counterReducer} from "../share/reducers/couter.reducer";
 import {NameListReducer} from "@share/reducers/nameList.reducer";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../../environments/environment";
 import nameListReducer = NameListReducer.nameListReducer;
 
 @NgModule({
@@ -24,7 +26,12 @@ import nameListReducer = NameListReducer.nameListReducer;
         ReactiveFormsModule,
         NgbButtonsModule,
         ShareComponentModule,
-        StoreModule.forRoot({count: counterReducer, nameList: nameListReducer})
+        StoreModule.forRoot({count: counterReducer, nameList: nameListReducer}),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25, // Retains last 25 states
+            logOnly: environment.production, // Restrict extension to log-only mode
+            autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+        }),
     ],
     providers: [],
     bootstrap: [AppComponent]
